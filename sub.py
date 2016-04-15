e37c74b67e7a61469ef7ddb9b8bd42755210c9b8
JulianHurst <julian.hurst@laposte.net>
2016-04-16 01:09:08 +0200

    lang switch and shebang

--- sub.py
+++ sub.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 import urllib.request
 import sys
-import os
+import os.path
 import hashlib
+
 def get_hash(name):
     readsize = 64 * 1024
     with open(name, 'rb') as f:
@@ -11,11 +13,32 @@ def get_hash(name):
         data += f.read(readsize)
     return hashlib.md5(data).hexdigest()
 
-req=urllib.request.Request("http://api.thesubdb.com/?action=search&hash="+get_hash(sys.argv[1]),headers={"User-Agent" : "SubDB/1.0 (subdb/0.1; http://github.com/JulianHurst/subdb"})
+def error():
+    print("USAGE : "+sys.argv[0]+" [-l lang] file")
+    sys.exit(-1)
+
+
+if len(sys.argv)!=4 and len(sys.argv)!=2:
+    error()
+if len(sys.argv)==4:
+    if sys.argv[1]=="-l":
+        lang=sys.argv[2]
+        filename=sys.argv[3]
+    else:
+        error()
+else:
+    lang="NA"
+    filename=sys.argv[1]
+req=urllib.request.Request("http://api.thesubdb.com/?action=languages",headers={"User-Agent" : "SubDB/1.0 (subdb/0.1; http://github.com/JulianHurst/subdb"})
 data=urllib.request.urlopen(req)
-print(data.read().decode('utf-8'))
-lang=input("Choose one of the available languages : ")
-req=urllib.request.Request("http://api.thesubdb.com/?action=download&hash="+get_hash(sys.argv[1])+"&language="+lang,headers={"User-Agent" : "SubDB/1.0 (subdb/0.1; http://github.com/JulianHurst/subdb"})
+langs=data.read().decode('utf-8')
+langs=langs.split(',')
+req=urllib.request.Request("http://api.thesubdb.com/?action=search&hash="+get_hash(filename),headers={"User-Agent" : "SubDB/1.0 (subdb/0.1; http://github.com/JulianHurst/subdb"})
 data=urllib.request.urlopen(req)
-with open(os.path.splitext(sys.argv[1])[0]+".srt","w+") as f:
-    f.write(data.read().decode('utf-8'))
+print("available languages : ",data.read().decode('utf-8'))
+while lang not in langs[:]:
+    lang=input("Choose one of the available languages : ")
+req=urllib.request.Request("http://api.thesubdb.com/?action=download&hash="+get_hash(filename)+"&language="+lang,headers={"User-Agent" : "SubDB/1.0 (subdb/0.1; http://github.com/JulianHurst/subdb"})
+data=urllib.request.urlopen(req)
+with open(os.path.splitext(filename)[0]+".srt","wb") as f:
+    f.write(data.read())
